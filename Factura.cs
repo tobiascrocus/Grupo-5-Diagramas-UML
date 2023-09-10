8962761aaf78bdcd2613458960bd3042eb0afa25
Jeremias91 <126523676+Jeremias91@users.noreply.github.com>
2023-09-09 22:22:58 -0300

    Add files via upload

---
+++ Factura.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class Factura
+{
+
+  #region Aggregations
+
+
+  #endregion
+
+  #region Compositions
+
+
+  #endregion
+
+  #region Attributes
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private int ID;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private Fecha fecha;
+
+
+  /// <summary>
+  /// 
+  /// </summary>
+  private double monto;
+
+
+
+  #endregion
+
+
+  #region Public methods
+
+  #endregion
+
+
+  #region Protected methods
+
+  #endregion
+
+
+  #region Private methods
+
+  #endregion
+
+
+}
+
